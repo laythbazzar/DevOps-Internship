172ad8fea060de0a742e3e207b326906d40cf670
laythbazzar <laythbazzar8@gmail.com>
2023-01-22 02:53:56 -0500

    task1 part2 modified

--- task1/part2.sh
+++ task1/part2.sh
@@ -5,12 +5,15 @@ useradd -u 601 -p redhat -s /sbin/nologin user1
 #create group:TrainingGroup
 groupadd TrainingGroup
 
-#modify user1: set group TrainingGroup
-usermod -g TrainingGroup user1
+#modify user1: append a suplementary group TrainingGroup
+usermod -a -G TrainingGroup user1
 
 #create group admin, password: redhat, create users: user2,user3 , group:admin, password:redhat
 groupadd -p redhat admin 
 adduser user2 -g admin -p redhat 
 adduser user3 -g admin -p redhat
+
+#give user3 root permission: set root group as suplementary group
+usermod -a -G root user3
 
 
