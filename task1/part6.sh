78bc40164014d5b72d102354e71e7c3fec5546f1
laythbazzar <laythbazzar8@gmail.com>
2023-01-29 09:23:09 -0500

    task1 part6 modification

--- task1/part6.sh
+++ task1/part6.sh
@@ -3,7 +3,7 @@
 sleep 600& #600 seconds=10 mins
 ps -aux |grep -w 'sleep'
 #get the id of sleep from active processes
-id=$(ps -u|grep -w 'sleep 600'|awk '(NR==1){print $2}')
+id=$(ps -u|grep -w 'sleep 600'|awk ' (NR>=1){l[NR-1]= $2} END{for (i=0;i <NR-1;i++) print l[i]} ')
 #print id of process then kill it
 
 echo "id is $id"
